74d46cab257870b7461333e9a5e55b86bcfdc453
yhjiujiu <1289571768@qq.com>
2025-07-08 10:19:44 +0800

    prompt

--- prompt_ensemble.py
+++ prompt_ensemble.py
@@ -3,13 +3,15 @@ from typing import Union, List
 from pkg_resources import packaging
 import torch
 import numpy as np
-from open_clip.simple_tokenizer import SimpleTokenizer as _Tokenizer
+#from open_clip.simple_tokenizer import SimpleTokenizer as _Tokenizer
+from AnomalyCLIP_lib.simple_tokenizer import SimpleTokenizer as _Tokenizer
 # from open_clip import tokenizer
 # simple_tokenizer = tokenizer.SimpleTokenizer()
 from copy import deepcopy
 import torch.nn as nn
 
 from collections import OrderedDict
+
 
 _tokenizer = _Tokenizer()
 
@@ -75,7 +77,7 @@ def _get_clones(module, N):
 # "pcb4",
 # "pipe_fryum",
 
-class FewVand_PromptLearner(nn.Module):
+class FewVand_PromptLearner_yh(nn.Module):
     def __init__(self, clip_model, n_ctx, device,Pparameters):
         super().__init__()
 
@@ -87,9 +89,7 @@ class FewVand_PromptLearner(nn.Module):
         self.num_query_tokens = Pparameters['num_query_tokens']
         self.width = Pparameters["width"]
         self.emb_dim = Pparameters["emb_dim"]
-        self.query_token =  nn.Parameter(
-    torch.randn(self.num_query_tokens, self.width).to(device)
-    ) 
+        self.query_token = nn.Parameter(torch.randn(self.num_query_tokens, self.width))
         self.image_proj = nn.Linear(self.width,self.emb_dim).to(device)
         self.classfier = nn.Linear(self.width,2).to(device)
 
@@ -97,8 +97,10 @@ class FewVand_PromptLearner(nn.Module):
         ctx_vectors_neg = torch.empty(n_ctx_neg, ctx_dim, dtype=dtype)
         nn.init.normal_(ctx_vectors_pos, std=0.02)
         nn.init.normal_(ctx_vectors_neg, std=0.02)
-        prompts_pos = " ".join(["X"] * n_ctx_pos)+"."
-        prompts_neg = " ".join(["X"] * n_ctx_neg)+'.'
+        prompts_pos = " ".join(["X"] * n_ctx_pos)+" "+"object"+"."
+        prompts_neg = " ".join(["X"] * n_ctx_neg)+" "+"damaged object"+'.'
+        print("prompts_pos:{}".format(prompts_pos))
+        print("prompts_neg:{}".format(prompts_neg))
 
         self.ctx_pos = nn.Parameter(ctx_vectors_pos.to(device))  # to be optimized
         self.ctx_neg = nn.Parameter(ctx_vectors_neg.to(device))  # to be optimized
@@ -134,9 +136,10 @@ class FewVand_PromptLearner(nn.Module):
         # print("shape", self.ctx_pos[0:1].shape, ctx_pos.shape)
         prefix_pos = self.token_prefix_pos
         prefix_neg = self.token_prefix_neg
-
+        #print("prefix_pos: {}".format(prefix_pos))
         suffix_pos = self.token_suffix_pos
         suffix_neg = self.token_suffix_neg
+        #print("suffix_pos: {}".format(suffix_pos))  
 
         print("prefix_pos:",prefix_pos.size(),ctx_pos.size(),suffix_pos.size())
 #prefix_pos: torch.Size([1, 1, 768])
@@ -146,28 +149,36 @@ class FewVand_PromptLearner(nn.Module):
         prompts_pos = torch.cat(
             [
                 # N(the number of template), 1, dim
-                prefix_pos.squeeze(0),  # ( 1, dim)
-                ctx_pos,  # (n_ctx, dim)
-                suffix_pos.squeeze(0),  # (n_cls, *, dim)
+                prefix_pos,  # ( 1, 1,dim)
+                ctx_pos.unsqueeze(0),  # (n_ctx, dim)
+                suffix_pos,  # (n_cls, *, dim)
             ],
-            dim=0,
+            dim=1,
         )
-
+        # print("prefix_pos.squeeze(0): {}".format(prefix_pos.squeeze(0)))
+        # print("ctx_pos: {}".format(ctx_pos))
+        # print("suffix_pos.squeeze(0): {}".format(suffix_pos.squeeze(0)))
+        # print("prompts_pos: {}".format(prompts_pos))
         prompts_neg = torch.cat(
             [
-                prefix_neg.squeeze(0),  
-                ctx_neg, 
-                suffix_neg.squeeze(0),  
+                prefix_neg,  
+                ctx_neg.unsqueeze(0), 
+                suffix_neg,  
             ],
-            dim=0,
+            dim=1,
         )
-        prompts = torch.stack((prompts_neg, prompts_pos), dim=0) #[2,77,768]
-        print("prompts:",prompts.size())
+        # print("prefix_neg.squeeze(0): {}".format(prefix_neg.squeeze(0)))
+        # print("ctx_neg: {}".format(ctx_neg))
+        # print("suffix_neg.squeeze(0): {}".format(suffix_neg.squeeze(0)))
+        # print("prompts_neg: {}".format(prompts_neg))
 
-        tokenized_prompts = torch.stack((self.tokenized_prompts_neg, self.tokenized_prompts_pos), dim=0) #[2,77]
+        prompts = torch.cat([prompts_neg, prompts_pos], dim=0) #[2,77,768]
+        print("prompts:",prompts)
+
+        tokenized_prompts = torch.cat([self.tokenized_prompts_neg, self.tokenized_prompts_pos], dim=0) #[2,77]
 
         print("tokenized_prompts:",tokenized_prompts.size())
-
+        print("tokenized_prompts:",tokenized_prompts)
         prompt = prompts
         tokenized_prompt = tokenized_prompts.squeeze(1)
 
@@ -190,3 +201,130 @@ class FewVand_PromptLearner(nn.Module):
             return text_prompts["objs"]
         else:
             return text_prompts
+
+class FewVand_PromptLearner(nn.Module):
+    def __init__(self, clip_model,Pparameters):
+        super().__init__()
+        classnames = ["object"]
+        self.n_cls = len(classnames)
+        self.tokenize = tokenize
+        n_ctx_pos = 10
+        n_ctx_neg = 10
+        dtype = clip_model.visual.conv1.weight.dtype
+
+        ctx_dim = clip_model.ln_final.weight.shape[0]
+        self.classnames = classnames
+
+        self.width = Pparameters["width"]
+        self.emb_dim = Pparameters["emb_dim"]
+        #self.image_proj = nn.Linear(self.width,self.emb_dim)
+
+
+        # Random Initialization
+        print("Initializing class-specific contexts")
+        # 这里是cls是类的个数，n_ctx_pos代表learnable token的长度，ctx_dim表示prompt的dimension
+        ctx_vectors_pos = torch.empty(self.n_cls, 1, n_ctx_pos, ctx_dim, dtype=dtype)
+        ctx_vectors_neg = torch.empty(self.n_cls, 1, n_ctx_neg, ctx_dim, dtype=dtype)
+        nn.init.normal_(ctx_vectors_pos, std=0.02)
+        nn.init.normal_(ctx_vectors_neg, std=0.02)
+        prompt_prefix_pos = " ".join(["N"] * n_ctx_pos)
+        prompt_prefix_neg = " ".join(["A"] * n_ctx_neg)
+        # self.compound_prompts_depth = design_details["learnabel_text_embedding_depth"]
+        # self.compound_prompts_text = nn.ParameterList([nn.Parameter(torch.empty(self.text_encoder_n_ctx, ctx_dim))
+        #                                                for _ in range(self.compound_prompts_depth - 1)])
+        # for single_para in self.compound_prompts_text:
+        #     print("single_para", single_para.shape)
+        #     nn.init.normal_(single_para, std=0.02)
+
+        # single_layer = nn.Linear(ctx_dim, 896)
+        # self.compound_prompt_projections = _get_clones(single_layer, self.compound_prompts_depth - 1)
+
+        self.ctx_pos = nn.Parameter(ctx_vectors_pos)  # to be optimized
+        self.ctx_neg = nn.Parameter(ctx_vectors_neg)  # to be optimized
+
+        classnames = [name.replace("_", " ") for name in classnames]
+        # name_lens = [len(_tokenizer.encode(name)) for name in classnames]
+
+        prompts_pos = [prompt_prefix_pos + " " + name + "." for name in classnames]
+        prompts_neg = [prompt_prefix_pos + " " + prompt_prefix_neg + " " + "damaged" + " " + name + "." for name in classnames]
+
+        tokenized_prompts_pos = []
+        tokenized_prompts_neg = []
+
+        for p_pos in prompts_pos:
+            tokenized_prompts_pos.append(self.tokenize(p_pos))
+        for p_neg in prompts_neg:
+            tokenized_prompts_neg.append(self.tokenize(p_neg))
+        tokenized_prompts_pos = torch.cat(tokenized_prompts_pos)
+        tokenized_prompts_neg = torch.cat(tokenized_prompts_neg)
+        # 生成相应的text embedding
+        with torch.no_grad():
+            embedding_pos = clip_model.token_embedding(tokenized_prompts_pos).type(dtype)
+            embedding_neg = clip_model.token_embedding(tokenized_prompts_neg).type(dtype)
+            n, l, d = embedding_pos.shape
+            print("embedding_pos", embedding_pos.shape)
+            embedding_pos = embedding_pos.reshape(1, self.n_cls, l, d).permute(1, 0, 2, 3)
+            embedding_neg = embedding_neg.reshape(1, self.n_cls, l, d).permute(1, 0, 2, 3)
+
+        self.register_buffer("token_prefix_pos", embedding_pos[:, :, :1, :])
+        self.register_buffer("token_suffix_pos", embedding_pos[:, :, 1 + n_ctx_pos:, :])
+        self.register_buffer("token_prefix_neg", embedding_neg[:, :, :1, :])
+        self.register_buffer("token_suffix_neg", embedding_neg[:, :, 1 + n_ctx_neg:, :])
+
+        n, d = tokenized_prompts_pos.shape
+        tokenized_prompts_pos = tokenized_prompts_pos.reshape(1, self.n_cls, d).permute(1, 0, 2)
+
+        n, d = tokenized_prompts_neg.shape
+        tokenized_prompts_neg = tokenized_prompts_neg.reshape(1, self.n_cls, d).permute(1, 0, 2)
+
+        self.n_ctx_pos = n_ctx_pos
+        self.n_ctx_neg = n_ctx_neg
+        self.vis_dim = ctx_dim
+        # tokenized_prompts = torch.cat([tokenized_prompts_pos, tokenized_prompts_neg], dim=0)  # torch.Tensor
+        self.register_buffer("tokenized_prompts_pos", tokenized_prompts_pos)
+        self.register_buffer("tokenized_prompts_neg", tokenized_prompts_neg)
+        print("tokenized_prompts shape", self.tokenized_prompts_pos.shape, self.tokenized_prompts_neg.shape)
+
+    def forward(self, cls_id=None):
+        ctx_pos = self.ctx_pos
+        # print("shape", ctx_pos.shape)
+        prefix_pos = self.token_prefix_pos
+        suffix_pos = self.token_suffix_pos
+
+        prompts_pos = torch.cat(
+            [
+                # N(the number of template), 1, dim
+                prefix_pos,  # (n_cls, 1, dim)
+                ctx_pos,  # (n_cls, n_ctx, dim)
+                suffix_pos,  # (n_cls, *, dim)
+            ],
+            dim=2,
+        )
+
+        ctx_neg = self.ctx_neg
+        prefix_neg = self.token_prefix_neg
+        suffix_neg = self.token_suffix_neg
+
+        prompts_neg = torch.cat(
+            [
+                # N(the number of template), 1, dim
+                prefix_neg,  # (n_cls, 1, dim)
+                ctx_neg,  # (n_cls, n_ctx, dim)
+                suffix_neg,  # (n_cls, *, dim)
+            ],
+            dim=2,
+        )
+
+        _, _, l, d = prompts_pos.shape
+        prompts_pos = prompts_pos.reshape(-1, l, d)
+        _, _, l, d = prompts_neg.shape
+        prompts_neg = prompts_neg.reshape(-1, l, d)
+        # prompts = torch.cat([prompts_pos, prompts_neg], dim=0)
+
+        _, l, d = self.tokenized_prompts_pos.shape
+        tokenized_prompts_pos = self.tokenized_prompts_pos.reshape(-1, d)
+        _, l, d = self.tokenized_prompts_neg.shape
+        tokenized_prompts_neg = self.tokenized_prompts_neg.reshape(-1, d)
+        # tokenized_prompts = torch.cat((tokenized_prompts_pos, tokenized_prompts_neg), dim = 0)
+
+        return prompts_pos, prompts_neg, tokenized_prompts_pos, tokenized_prompts_neg
